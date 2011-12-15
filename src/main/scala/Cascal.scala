de38e125ac6bc051302d7120e755d33a22ce4756
Lanny Ripple <Lanny.Ripple@gmail.com>
2011-12-15 15:14:50 -0600

    cascal example

--- src/main/scala/Cascal.scala
+++ src/main/scala/Cascal.scala
@@ -1,3 +1,20 @@
-class Cascal {
+package me.remi.cass
 
+import com.shorrockin.session._
+
+object Cass {
+
+    val hosts = Host("localhost", 9160, 30 * 1000L) :: Nil
+    val params = new PoolParams(200, ExhaustionPolicy.Fail, 500L, 6, 2)
+    lazy val pool = new SessionPool(hosts, params, Consistency.One)
+}
+
+object Example {
+
+    def main(av: Array[String]) {
+        val cols = Cass.pool.borrow {
+            session.list( Keyspace("Trail") \ "People" \ "1" )
+        }
+        println cols.mkString("\n")
+    }
 }
